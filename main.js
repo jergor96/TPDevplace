ce162803e02c360fe17ced2d988d4d5774e71c91
jergor96 <37788600+jergor96@users.noreply.github.com>
2021-12-11 18:41:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let = document.getElementById("demo");
+
+function mifuncion(){
+    alert("Esto es una alerta con java script");
+}
+function alerta(){
+    alert("Esta seccion aun no esta disponible")
+}
+
+
---
+++ sw.js
@@ -0,0 +1,10 @@
+function swfuncion(){
+    Swal.fire({
+        title: 'Lo siento!',
+        text: 'Esta opcion aun no esta disponible',
+        imageUrl: 'https://c.tenor.com/MabZbm3kIHMAAAAC/anime-nervous.gif',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+}
